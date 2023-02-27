f6e7cec0e49c7508587506613220f13f138f44a5
Garth Gilmour <garth.gilmour@instil.co>
2023-02-27 15:25:17 +0000

    Add demo of windows with menu bars

---
+++ examples/DesktopComposeInDepth/src/jvmMain/kotlin/desktop/windows/WindowsApp.kt
@@ -0,0 +1,20 @@
+package desktop.windows
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun WindowsApp() {
+    MaterialTheme {
+        Row(modifier = Modifier.padding(all = 20.dp)) {
+            Text("Sample Text")
+        }
+    }
+}
